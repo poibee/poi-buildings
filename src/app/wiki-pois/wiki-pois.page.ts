3f52c5aa533419faf14d0f803821d3ddfc9bf0ea
Poibee <info@poibee.org>
2025-02-20 23:20:59 +0100

    feat: add responsive grid with famous buildings cards

--- src/app/wiki-pois/wiki-pois.page.ts
+++ src/app/wiki-pois/wiki-pois.page.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-wiki-pois',
   templateUrl: './wiki-pois.page.html',
   styleUrls: ['./wiki-pois.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent, CommonModule, FormsModule]
 })
 export class WikiPoisPage implements OnInit {
+  famousBuildings = [
+    { name: 'Eiffel Tower', location: 'Paris, France' },
+    { name: 'Colosseum', location: 'Rome, Italy' },
+    { name: 'Taj Mahal', location: 'Agra, India' },
+    { name: 'Sydney Opera House', location: 'Sydney, Australia' },
+    { name: 'Empire State Building', location: 'New York, USA' },
+    { name: 'Sagrada Familia', location: 'Barcelona, Spain' },
+    { name: 'Burj Khalifa', location: 'Dubai, UAE' },
+    { name: 'Big Ben', location: 'London, UK' },
+    { name: 'Petronas Towers', location: 'Kuala Lumpur, Malaysia' },
+    { name: 'St. Basil\'s Cathedral', location: 'Moscow, Russia' },
+    { name: 'Christ the Redeemer', location: 'Rio de Janeiro, Brazil' },
+    { name: 'Great Wall of China', location: 'China' }
+  ];
 
   constructor() { }
 
